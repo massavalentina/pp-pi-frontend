c0a9620547d5149436b4d02efe9375f850ff5bfd
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2025-07-22 14:16:23 -0300

    Arreglo de las modificaciones previas
    Las modificaciones realizadas previamente hicieron que los botones desapareciesen por lo que se arregló configurando nuevamente routerLink y router-outlet.
    Solo queda redefinir la ruta para home, ya que repite el menú.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherforecastService } from './weatherforecast.service';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { Ruta1Component } from './pages/ruta1/ruta1.component';
 import { Ruta2Component } from './pages/ruta2/ruta2.component';
 import { Ruta3Component } from './pages/ruta3/ruta3.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: AppComponent },
   { path: 'ruta1', component: Ruta1Component },
   { path: 'ruta2', component: Ruta2Component },
   { path: 'ruta3', component: Ruta3Component },
