a3a8c2499c7566e8f8ed100cc59fb460e4c56de9
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2025-07-31 12:47:43 -0300

    Actualización a Angular 20 - SweetAlert
    Por una serie de errores en instalación de ngx SweetAlert2 (no soporta en Angular20), se intentó actualizar todo a Angular 20.
    
    Por el lado de SweetAlert, en vez de importalo con el ngx, se decidió importar el Swal desde cada componente después de hacer npm install sweetalert2.
    
    En componente de ruta2, se importa el Swal y se utiliza en el método Delete().
    
    En el HTML se agrega el botón de confirm, para que la alerta funcione de manera correcta.
    
    Se testeó y funciona.

--- src/app/pages/ruta2/ruta2.component.ts
+++ src/app/pages/ruta2/ruta2.component.ts
@@ -4,7 +4,8 @@ import { RouterLink } from '@angular/router';
 import { AuthorsService } from '../../authors.service';
 import { IAuthors } from './authors.models';
 import { MatTableModule} from '@angular/material/table';
-import { NgIf} from '@angular/common';
+import Swal from 'sweetalert2';
+
 
 
 @Component({
@@ -30,9 +31,26 @@ export class Ruta2Component {
     .subscribe(authors =>{this.authors = authors;})
   }
 
-  Delete(id:number){
-    this.authorService.Delete(id).subscribe(()=>{});
-    this.LoadAuthors();
-  }
-  
-}
+  Delete(id:number):void{
+    Swal.fire({      
+      title: '¿Desea eliminar el registro?', // Title for the alert
+      text: 'Esta acción no se puede deshacer', // Text of the alert
+      icon: 'warning', // The big icon - Warn
+      showCancelButton: true, // Cancellation enabled
+      confirmButtonText: 'Sí, deseo eliminar el registro', // Confirmation button
+      cancelButtonText: 'Cancelar' // Cancellation button
+    })
+    
+    .then((result)=>{ 
+      if (result.isConfirmed) // Executes if the user confirm the deletion
+        {this.authorService.Delete(id).subscribe(()=>{ // Executes the deletion
+          Swal.fire({
+            icon: 'success', // The big icon - Success
+            title: 'Registro eliminado', // The message
+            text: 'El registro fue eliminado correctamente.',
+            timer: 100,
+            showConfirmButton: false
+              
+          });
+          this.LoadAuthors(); // Load the table of authors again
+        })}})}}
