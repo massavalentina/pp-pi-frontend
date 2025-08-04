eb89598d4978ec8a2d7561f30446b095108f4c1a
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2025-08-04 12:13:39 -0300

    Edición del componente menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherforecastService } from './weatherforecast.service';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
